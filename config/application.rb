705eefe2c695b4c8b3197b15210cfd3c3dda7506
Christoph Thiel <ct@kki.org>
2011-07-27 21:50:41 +0200

    set default generators

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,13 @@ Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module Simplekanban
   class Application < Rails::Application
+  
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.integration_tool :rspec, :fixture => true, :views => true
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
