bceb6822e9a585fb1a0597a11b8000c68a903c5a
Christoph Thiel <ct@kki.org>
2011-07-27 21:07:02 +0200

    add acts-as-taggable-on for tags on stories

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ end
 
 gem 'jquery-rails'
 gem 'formtastic'
+gem 'acts-as-taggable-on'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/story.rb
+++ app/models/story.rb
@@ -2,6 +2,8 @@ class Story < ActiveRecord::Base
   validates :number, :presence => true, :uniqueness => true 
   validates :name, :presence => true, :uniqueness => true
   validates :status, :presence => true, :associated => true
+  
+  acts_as_taggable_on :tags
 
   belongs_to :status
 
