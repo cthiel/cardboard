a7bf933c305a3337f39000ea1cd212b75ebca7c3
Christoph Thiel <ct@kki.org>
2011-07-28 11:28:43 +0200

    add stories/edit.js.haml for inline editing [#issue 9]

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -31,6 +31,11 @@ class StoriesController < ApplicationController
   # GET /stories/1/edit
   def edit
     @story = Story.find(params[:id])
+
+    respond_to do |format|
+      format.html # edit.html.haml
+      format.js   # edit.js.haml
+    end
   end
 
   # POST /stories
