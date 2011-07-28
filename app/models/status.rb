666fcc1c27d5a7b651d819e1fc70e559c80aabc2
Christoph Thiel <ct@kki.org>
2011-07-28 14:41:38 +0200

    remove autogeneration of ready statuses

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,14 +3,4 @@ class Status < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
 
   has_many :stories
-
-  after_create :create_ready_status
-
-private
-
-  def create_ready_status
-    unless self.code.include?('_Q')
-      Status.create!(:name => self.name + " Ready", :code => self.code + "_Q", :color => '#F0F0F0')
-    end
-  end  
 end
