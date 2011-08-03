bfbb5c306e5e8769106a78b24d535bc64ca1d809
Garrett LeSage <garrett@lesage.us>
2011-08-03 11:59:03 +0200

    make the default card a little more positive (:

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ Welcome to Cardboard!
 
 Cardboard is a simple way to keep simple information in simple categories, and share it, simply. (We like simplicity).
 
-Double-click on this card to edit it, or drag it into a different column.  Double-click the blank space in a column to create a new card.  The text uses [Markdown](http://daringfireball.net/projects/markdown/syntax), a dead-simple way to add some **basic** _decoration_.  The first line is your title.  No fancy fields... just your notes on cards.  
+Double-click on this card to edit it, or drag it into a different column.  Double-click the blank space in a column to create a new card.  The text uses [Markdown](http://daringfireball.net/projects/markdown/syntax), a super-simple way to add some **basic** _decoration_.  The first line is your title.  No fancy fields... just your notes on cards.
 
 We're using it for kanban.  Feel free to use it to get those silly notes off your monitor. Or whatever :)
 
