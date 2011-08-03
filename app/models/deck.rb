eec610fe6efcaf91f5bfae0123e229df9c4e26d4
James Mason <jmason@suse.com>
2011-08-03 09:12:32 -0700

    Fixing up acts_as_list on decks.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,11 +1,21 @@
 class Deck < ActiveRecord::Base
+  before_validation :position_last_if_null
   validates :name, :presence => true, :uniqueness => true
   validates :board, :presence => true, :associated => true
+  validates :position, :numericality => true
   
   belongs_to :board
   has_many :cards
   has_paper_trail
-  acts_as_list
+  acts_as_list :scope => :board
   
   default_scope order("position")
+
+  protected
+  
+  def position_last_if_null
+    unless self.position
+      self.position = Deck.count + 1
+    end 
+  end  
 end
