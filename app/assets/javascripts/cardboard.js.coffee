cthiel/cardboard

commit 3d9e160a5707978c795f6a17f8db7811de8fbee9

Remove a deck.

app/assets/javascripts/cardboard.js.coffee
@@ -19,9 +19,10 @@
       reloadDecksCSS()
       initCards()
     
-    $('.new_deck_control').click () ->
+    $('.control.new_deck').click () ->
       showNewDeckDialog()
     
+    $('.control.delete_deck').live "click", removeDeck
 
   initCards = (cards) ->
     board = appData.board = {}
@@ -34,6 +35,7 @@
         board[deck].push datum.card
 
       createBoard appData
+      
 
 
   reloadDecksCSS = ->
@@ -144,7 +146,6 @@
           """
 
         $cardElement.data "card", card
-
         $list.append $cardElement
 
     $list
@@ -186,10 +187,16 @@
 
 
   createColumn = (board, deck, headline) ->
-    $("<div class='column' id='deck_#{deck}'></div>")
+    if board[deck]?.length > 0
+      used = " in_use"
+    else
+      used = " empty"
+  
+    $("<div class='column#{used}' id='deck_#{deck}'></div>")
+      .append("<div class='control delete_deck' title='Remove this deck'>&#215;</div>")
       .append("<h2 class='name'>#{headline}</h2>")
       .append(createList board, deck)
-
+        
       .data("deck", deck)
 
       .delegate '.box', 'dblclick', (e) ->
@@ -236,6 +243,7 @@
   updateCardDeck = (e, drag) ->
     $item = drag.item
     $box = $item.find '.box'
+    $from_column = drag.placeholder.closest(".column")
     cardId = $item.data("card").id
     deckId = $item.parent()[0].id.replace('deck_','')
 
@@ -248,6 +256,19 @@
       complete: ->
         $box.removeClass "unsaved"
         clearStatus()
+        initDecks()
+
+
+  removeDeck = (event) ->
+    $deck = $(event.target).closest(".column")
+    deck_id = $deck.attr("id").replace("deck_","")
+    deck_title = $deck.find("h2").text()
+    if confirm "Remove the '#{deck_title}' deck?"
+      $.ajax
+        type: "DELETE"
+        url: "/decks/#{deck_id}"
+        complete: $deck.effect("drop", 2000, initDecks)
+          
 
 
   init = ->

app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require 'ui/jquery.ui.draggable'
 //= require 'ui/jquery.ui.sortable'
 //= require 'ui/jquery.ui.dialog'
+//= require 'ui/jquery.effects.core'
+//= require 'ui/jquery.effects.drop'
 //= require 'showdown'
 //= require 'jquery.editable'

app/assets/javascripts/ui/jquery.effects.drop.js
@@ -1,0 +1,50 @@
+/*
+ * jQuery UI Effects Drop 1.8.14
+ *
+ * Copyright 2011, AUTHORS.txt (http://jqueryui.com/about)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * http://docs.jquery.com/UI/Effects/Drop
+ *
+ * Depends:
+ *	jquery.effects.core.js
+ */
+(function( $, undefined ) {
+
+$.effects.drop = function(o) {
+
+	return this.queue(function() {
+
+		// Create element
+		var el = $(this), props = ['position','top','bottom','left','right','opacity'];
+
+		// Set options
+		var mode = $.effects.setMode(el, o.options.mode || 'hide'); // Set Mode
+		var direction = o.options.direction || 'left'; // Default Direction
+
+		// Adjust
+		$.effects.save(el, props); el.show(); // Save & Show
+		$.effects.createWrapper(el); // Create Wrapper
+		var ref = (direction == 'up' || direction == 'down') ? 'top' : 'left';
+		var motion = (direction == 'up' || direction == 'left') ? 'pos' : 'neg';
+		var distance = o.options.distance || (ref == 'top' ? el.outerHeight({margin:true}) / 2 : el.outerWidth({margin:true}) / 2);
+		if (mode == 'show') el.css('opacity', 0).css(ref, motion == 'pos' ? -distance : distance); // Shift
+
+		// Animation
+		var animation = {opacity: mode == 'show' ? 1 : 0};
+		animation[ref] = (mode == 'show' ? (motion == 'pos' ? '+=' : '-=') : (motion == 'pos' ? '-=' : '+=')) + distance;
+
+		// Animate
+		el.animate(animation, { queue: false, duration: o.duration, easing: o.options.easing, complete: function() {
+			if(mode == 'hide') el.hide(); // Hide
+			$.effects.restore(el, props); $.effects.removeWrapper(el); // Restore
+			if(o.callback) o.callback.apply(this, arguments); // Callback
+			el.dequeue();
+		}});
+
+	});
+
+};
+
+})(jQuery);

app/assets/stylesheets/cardboard.css.sass
@@ -126,14 +126,18 @@ nav
     ul:hover
       background-color: #ffc
 
-.new_deck_control
-  width: 5%
+.control
   float: right
+  color: #babdb6
   text-align: center
   font-size: 200%
   cursor: pointer
   &:active, &:hover
     color: orange  
+  &.new_deck
+    width: 5%
+  &.delete_deck
+    padding: 0 0.5em
   
 .column
   +box-sizing
@@ -142,9 +146,12 @@ nav
   float: left
   width: 10%
   height: 300px
-
+  &.in_use
+    .control.delete_deck
+      visibility: hidden
+      
   h2
-    font-size: 12px
+    font-size: 100%
     font-weight: normal
     color: #000
     margin: 6px

app/controllers/cards_controller.rb
@@ -66,10 +66,16 @@ class CardsController < ApplicationController
   # DELETE /cards/1
   def destroy
     @card = Card.find(params[:id])
-    @card.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(cards_url) }
+    if @card.destroy
+      respond_to do |format|
+        format.html { redirect_to(cards_url, :notice => 'Card was deleted.') }
+        format.json { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { render :action => "edit" }
+        format.json { render :json => @card.errors, :deck => :unprocessable_entity }
+      end
     end
   end
 end

app/views/boards/show.html.haml
@@ -3,4 +3,4 @@
   = javascript_include_tag 'cardboard'
   = stylesheet_link_tag decks_path(:format => 'css'), :id => "decks_css"
 %div#board
-  %div.new_deck_control(title="Add a new deck") +
+  %div.control.new_deck(title="Add a new deck") +

commit 6bfd37b5ad623cf53682179ee5c6a11f03241fc8

fixed card saving (commit 3d9e160 introduced a bunch of bugs)

app/assets/javascripts/cardboard.js.coffee
@@ -228,7 +228,7 @@
         drag.placeholder.height drag.item.height()
       stop: (e, drag) ->
         unlockScrollbars()
-        updateCardDeck e, drag
+        saveCards e, drag
 
     $(".column", $columns)
       .disableSelection()
@@ -237,10 +237,9 @@
     $('#columns').replaceWith($columns)
 
 
-  updateCardDeck = (e, drag) ->
+  saveCards = (e, drag) ->
     $item = drag.item
     $box = $item.find '.box'
-    $from_column = drag.placeholder.closest(".column")
     cardId = $item.data("card").id
     deckId = $item.parent()[0].id.replace('deck_','')
 
@@ -253,7 +252,6 @@
       complete: ->
         $box.removeClass "unsaved"
         clearStatus()
-        initDecks()
 
 
   removeDeck = (event) ->