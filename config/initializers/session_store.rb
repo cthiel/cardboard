ff78faec4c865ccede9bc729e2e3901244aed11d
Christoph Thiel <ct@kki.org>
2011-09-05 19:04:44 +0200

    update configs to rails 3.1 release

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CardBoard::Application.config.session_store :cookie_store, :key => '_cardboard_session'
+CardBoard::Application.config.session_store :cookie_store, :key => '_card_board_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
